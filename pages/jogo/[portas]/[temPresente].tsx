ca6b898ecb4825152bea4722252b88bb7323a912
Luigi Nicoletti <callmenicoletti@gmail.com>
2022-06-09 22:28:18 -0300

    FIX - Validação das entradas.
    Validação das entradas:
    
    Porta com presente não pode ser superior ao numero de portas.

--- pages/jogo/[portas]/[temPresente].tsx
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -2,18 +2,29 @@ import styles from "../../../styles/Jogo.module.css";
 import { useEffect, useState } from "react";
 import Porta from "../../../components/Porta";
 import { atualizarPortas, criarPortas } from "../../../functions/portas";
-import Link from 'next/link';
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function jogo() {
   const router = useRouter();
   const [portas, setPortas] = useState([]);
+  const [valida, setValida] = useState(false);
 
   useEffect(() => {
-    const portas = +router.query.portas
-    const temPresente = +router.query.temPresente
+    const portas = +router.query.portas;
+    const temPresente = +router.query.temPresente;
     setPortas(criarPortas(portas, temPresente));
-  },[router?.query])
+  }, [router?.query]);
+
+  useEffect(() => {
+    const portas = +router.query.portas;
+    const temPresente = +router.query.temPresente;
+
+    const validaPorta = portas >= 2 && portas <= 100;
+    const validaPresente = temPresente >= 1 && temPresente <= portas;
+
+    setValida(validaPorta && validaPresente);
+  }, [portas]);
 
   function renderizarPortas() {
     return portas.map((porta) => {
@@ -31,7 +42,9 @@ export default function jogo() {
 
   return (
     <div id={styles.jogo}>
-      <div className={styles.portas}>{renderizarPortas()}</div>
+      <div className={styles.portas}>
+        {valida ? renderizarPortas() : <h1>Valores Inválidos</h1>}
+      </div>
       <div className={styles.menu}>
         <Link href="/">
           <button>Reiniciar</button>
